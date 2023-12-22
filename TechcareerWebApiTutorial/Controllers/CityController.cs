7f34360ffff2d4c2c21bb2f712dfff39869a8323
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-12-22 21:05:16 +0300

    Add project files.

---
+++ TechcareerWebApiTutorial/Controllers/CityController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechcareerWebApiTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        [HttpGet]
+        public string[] GetCities()
+        {
+            string[] cities = new string[] { "Istanbul", "Ankara", "Izmir", "Bursa", "Antalya" };
+            return cities;
+        }
+    }
+}
---
+++ TechcareerWebApiTutorial/Controllers/CountriesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechcareerWebApiTutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GetCountries()
+        {
+            List<string> countries = new List<string>
+            {
+                "Ulke1","Ulke2","Ulke3","Ulke4","Ulke5","Ulke6","Ulke7","Ulke8","Ulke9","Ulke10","Ulke11","Ulk12","Ulke13","Ulke14","Ulke15","Ulke16","Ulke17",
+                "Ulke18","Ulke10","Ulke20"
+            };
+            return Ok(countries);
+        }
+    }
+}
