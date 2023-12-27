1922b6c5c25d0148c2af2e32157e75dc9d0324c5
Kadir Şehmus Arslan <83732666+kadirarsln@users.noreply.github.com>
2023-12-27 14:55:48 +0300

    HttpPut added.

--- TechcareerWebApiTutorial/Controllers/EmployeeController.cs
+++ TechcareerWebApiTutorial/Controllers/EmployeeController.cs
@@ -27,7 +27,7 @@ namespace TechcareerWebApiTutorial.Controllers
         {
 
             //firstordefault metodu icerisine yazdığımız sorguya göre arama yapar
-            var employee = _context.Employees.FirstOrDefault(x => x.Id == id);
+            var employee = _context.Employees.FirstOrDefault(e=> e.Id == id);
 
             //blog yoksa 404 döndür
             if (employee == null)
@@ -48,11 +48,38 @@ namespace TechcareerWebApiTutorial.Controllers
             return StatusCode(201, employee);
         }
 
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Employee updatedEmployee)
+        {
+            // Veritabanında id'ye sahip bir Employee var mı kontrol et
+            var employee = _context.Employees.FirstOrDefault(e => e.Id == id);
+
+            // Employee bulunamadıysa 404 Not Found döndür
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            // Var olan Employee'in özelliklerini güncelle
+            employee.FirstName = updatedEmployee.FirstName;
+            employee.LastName = updatedEmployee.LastName;
+            employee.Adress = updatedEmployee.Adress;
+            employee.BirthDate = updatedEmployee.BirthDate;
+            employee.City = updatedEmployee.City;
+            employee.AddDate = updatedEmployee.AddDate;
+
+            _context.SaveChanges();
+
+            // Başarılı bir şekilde güncellendiğine dair 200 OK döndür
+            return Ok(employee);
+        }
+
+
         //delete
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
-            var employee = _context.Employees.FirstOrDefault(x => x.Id == id);
+            var employee = _context.Employees.FirstOrDefault(e=> e.Id == id);
 
             if (employee == null)
             {
